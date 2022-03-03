c42c90feeab60c420abbb48bf92efdc0087dbf03
Ayann12 <tantowi818@gmail.com>
2022-03-03 12:52:27 +0700

    kedua dong breyyy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Hasil from "./components/Hasil";
+import ListCategories from "./components/ListCategories";
 import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <div className="mt-3">
+        <Container fluid>
+          <Row>
+            <ListCategories />
+            <Col>
+              <h4><strong>Daftar Products</strong></h4>
+              <hr />
+            </Col>
+            <Hasil />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Nav, Navbar, NavDropdown, Container, Form, Button } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap'
 
 const NavbarComponent = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home"><strong>Kasirku</strong> APP</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
